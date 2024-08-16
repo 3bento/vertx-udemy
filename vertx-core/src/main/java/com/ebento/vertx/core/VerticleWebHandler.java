78d877190ccfa23cbe39f4b2acebc65ba9dbbef3
kenny <duh747@gmail.com>
2024-08-15 23:16:56 -0300

    Rename my handler

--- vertx-core/src/main/java/com/ebento/vertx/core/VerticleWebHandler.java
+++ vertx-core/src/main/java/com/ebento/vertx/core/VerticleWebHandler.java
@@ -4,11 +4,11 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 
-public class MainVerticle extends AbstractVerticle {
+public class VerticleWebHandler extends AbstractVerticle {
 
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new MainVerticle());
+    vertx.deployVerticle(new VerticleWebHandler());
   }
 
   @Override
