82e310ec889c972a198266c495e8c3550b010162
kenny <duh747@gmail.com>
2024-08-15 22:55:13 -0300

    Created my first Verticle Web App

---
+++ vertx-core/src/main/java/com/ebento/vertx/core/MainVerticle.java
@@ -0,0 +1,31 @@
+package com.ebento.vertx.core;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+//  Logger logger = Logger.getLogger(getClass().getName());
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888).onComplete(http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
